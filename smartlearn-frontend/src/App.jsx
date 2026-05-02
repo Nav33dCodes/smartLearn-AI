ec01ae6bd80a1f03bbd84c1a97c08a2c330fe85c
Naveed Ahmed <iamnaveed.cs@gmail.com>
2026-05-02 18:01:13 +0500

    Updated

--- smartlearn-frontend/src/App.jsx
+++ smartlearn-frontend/src/App.jsx
@@ -9,6 +9,7 @@ import InputBox from "./components/InputBox";
 
 // const API = "http://localhost:8000";
 //  const API = "https://smartlearn-ai-production.up.railway.app";
+// const API = import.meta.env.VITE_API_URL;
 const API = import.meta.env.DEV
   ? "http://localhost:8000"          // npm run dev
   : "https://smartlearn-ai-production.up.railway.app";  // vercel build
