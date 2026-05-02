042c44c53e3860597bbef4863d9d90d0a73fcaae
Naveed Ahmed <iamnaveed.cs@gmail.com>
2026-05-02 17:40:05 +0500

    updated

--- smartlearn-backend/main.py
+++ smartlearn-backend/main.py
@@ -19,7 +19,7 @@ def root():
 # ------------------------
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://smartlearn-ai-liard.vercel.app"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
