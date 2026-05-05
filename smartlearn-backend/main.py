ef1e43b039b611b43b7518caff4bddeead90287e
Naveed Ahmed <iamnaveed.cs@gmail.com>
2026-05-05 12:36:52 +0500

    true to false and *

--- smartlearn-backend/main.py
+++ smartlearn-backend/main.py
@@ -31,12 +31,15 @@ app = FastAPI(title="SmartLearn AI", version="9.2.2")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://smart-learn-ai-gules.vercel.app",
-        "http://localhost:5173",
-        "http://localhost:3000",
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
+
+    # allow_origins=[
+    #     "https://smart-learn-ai-gules.vercel.app",
+    #     "http://localhost:5173",
+    #     "http://localhost:3000",
+    # ],
+  
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
